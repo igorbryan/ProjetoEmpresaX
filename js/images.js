aff18ac115aaa4c38dbaa94d69afa97486146e8f
Igor Bryan <igorbmcruz@gmail.com>
2020-11-21 20:00:31 -0300

    Projeto

---
+++ js/images.js
@@ -0,0 +1,9 @@
+function before(){ 
+    document.getElementById('myImage') 
+    .src="img.png";
+} 
+  
+function afterr(){ 
+    document.getElementById('myImage') 
+    .src="/assets/pexels-photo-288477.png";
+}
